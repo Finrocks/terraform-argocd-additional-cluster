b192d2b095235e294106a9371dacb669330e96b0
babebort <89022219+babebort@users.noreply.github.com>
2022-11-09 00:27:58 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ locals {
   namespace                   = var.namespace
   insecure                    = var.insecure
   ca_data                     = base64decode(one(data.aws_eks_cluster.cluster[*].certificate_authority[0].data))
-  eks_cluster_id              = try(data.aws_eks_cluster.cluster.id)
+  eks_cluster_id              = try(data.aws_eks_cluster.cluster[*].id)
   argocd_endpoint             = one(data.aws_eks_cluster.cluster[*].endpoint)
 }
 
