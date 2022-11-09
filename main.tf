6004adc8a72297c7a9f0fc3e148dc811fef4d8a3
babebort <babebort@gmail.com>
2022-11-09 18:01:07 +0000

    new ver

--- main.tf
+++ main.tf
@@ -2,10 +2,7 @@ locals {
   enabled                     = module.this.enabled
   namespace                   = module.this.namespace
   insecure                    = var.insecure
-  ca_data                     = base64decode(one(data.aws_eks_cluster.cluster[*].certificate_authority[0].data))
-  ca_data1                     = one(data.aws_eks_cluster.cluster[*].certificate_authority[0].data)
-  ca_data2                     = one(data.aws_eks_cluster.cluster[*].certificate_authority[0])
-  ca_data3                    = base64decode(data.aws_eks_cluster.cluster[*].certificate_authority.0.data)
+  ca_data                     = base64decode(one(data.aws_eks_cluster.cluster.certificate_authority[0].data))
   eks_cluster_id              = one(data.aws_eks_cluster.cluster[*].id)
   argocd_endpoint             = one(data.aws_eks_cluster.cluster[*].endpoint)
 }
@@ -20,16 +17,4 @@ data "aws_eks_cluster" "cluster" {
 
 output "ca_data" {
   value = local.ca_data
-}
-
-output "ca_data1" {
-  value = local.ca_data1
-}
-
-output "ca_data2" {
-  value = local.ca_data2
-}
-
-output "ca_data3" {
-  value = local.ca_data3
 }
