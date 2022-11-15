eb73fcb2602a602903b6736a26a0ae069ef38c92
babebort <babebort@gmail.com>
2022-11-15 04:10:39 +0000

    fix: fix bug

--- main.tf
+++ main.tf
@@ -7,41 +7,8 @@ locals {
   argocd_endpoint             = one(data.aws_eks_cluster.cluster[*].endpoint)
 }
 
-
 data "aws_eks_cluster" "cluster" {
   count = local.enabled ? 1 : 0
   
   name = var.eks_cluster_id
-}
-
-output "z" {
-  value = base64decode(data.aws_eks_cluster.cluster[*].certificate_authority[0].data)
-}
-
-output "zz" {
-  value = base64decode(one(data.aws_eks_cluster.cluster[*].certificate_authority[*].data))
-}
-
-output "zzzz" {
-  value = base64decode(one(data.aws_eks_cluster.cluster[0]))
-}
-
-output "zzzzzx" {
-  value = base64decode(one(data.aws_eks_cluster.cluster[*]))
-}
-
-output "xz" {
-  value = base64decode(data.aws_eks_cluster.cluster[0])
-}
-
-output "xvz" {
-  value = base64decode(data.aws_eks_cluster.cluster[*])
-}
-
-output "bb" {
-  value = data.aws_eks_cluster.cluster[*]
-}
-
-output "bbb" {
-  value = data.aws_eks_cluster.cluster[0]
 }
