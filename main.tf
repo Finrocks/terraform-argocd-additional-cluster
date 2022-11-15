b0f69e66c4b1def3423dbce231ac58e05b127990
babebort <babebort@gmail.com>
2022-11-15 03:44:34 +0000

    fix: fix bug

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ locals {
   namespace                   = local.enabled && var.namespace
   insecure                    = local.enabled && var.insecure
   ca_data                     = local.enabled && base64decode(one(data.aws_eks_cluster.cluster[*].certificate_authority[0].data))
-  eks_cluster_id              = local.enabled ? one(data.aws_eks_cluster.cluster[*].id)
+  eks_cluster_id              = local.enabled && one(data.aws_eks_cluster.cluster[*].id)
   argocd_endpoint             = local.enabled && one(data.aws_eks_cluster.cluster[*].endpoint)
 }
 
